de57cea4fd878e5e4ae53ff3df6de3c19faaff42
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2025-11-11 13:19:37 -0300

    feat: add authentication with better auth

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -7,6 +7,12 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service }: ServiceItemProps) => {
+
+  const formatPrice = (service.priceInCents / 100).toLocaleString("PT-BR", {
+    style: "currency",
+      currency: "BRL",
+  })
+
   return (
     <div className="bg-zinc-100 flex items-center gap-4 rounded-lg border-1 p-3">
       <div className="relative h-25 w-25 flex-shrink-0">
@@ -25,7 +31,7 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
 
         <div className="mt-2 flex items-center justify-between">
           <h3 className="font-bold">
-            R$ {(service.priceInCents / 100).toFixed(2)}
+            {formatPrice}
           </h3>
           <Badge className="w-24 h-8 hover:bg-primary/60 cursor-pointer transition">
             Reservar
